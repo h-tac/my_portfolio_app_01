067a3c577471aafd79b37358a2ea2a6622955a32
TACHIBANA Hiroki <h.tachi.bana.04@gmail.com>
2023-09-06 23:36:44 +0900

    現在地へ移動するカスタムコントロール

--- app/javascript/google_maps_script.js
+++ app/javascript/google_maps_script.js
@@ -23,9 +23,14 @@ function hideLoadingMessage() {
   const loadingMessage = document.getElementById('loadingMessage');
   loadingMessage.style.display = 'none';
   loadingMessage.style.zIndex = '';
+  loadingMessage.innerText = "Loading map";
 
   clearInterval(loadingInterval);
 }
+
+
+
+
 
 // マップを表示する関数
 function initMap() {
@@ -33,6 +38,7 @@ function initMap() {
 
   let map = new google.maps.Map(document.getElementById('map'), {
     zoom: 15,
+    fullscreenControl: false,
   });
 
   // マーカーをマップの特定の位置に追加する関数
@@ -78,4 +84,46 @@ function initMap() {
 
     hideLoadingMessage();
   }
+
+
+
+  // 現在地へ移動するカスタムコントロール
+  const currentLocationButton = document.createElement('button');
+
+  currentLocationButton.style.backgroundColor = '#fff';
+  currentLocationButton.style.border = '2px solid #fff';
+  currentLocationButton.style.borderRadius = '3px';
+  currentLocationButton.style.boxShadow = '0 2px 6px rgba(0,0,0,.3)';
+  currentLocationButton.style.color = "rgb(25,25,25)";
+  currentLocationButton.style.cursor = 'pointer';
+  currentLocationButton.style.lineHeight = "28px";
+  currentLocationButton.style.marginRight = '10px';
+  currentLocationButton.style.marginTop = '10px';
+  currentLocationButton.style.textAlign = 'center';
+  currentLocationButton.title = '現在地へ移動';
+  currentLocationButton.className = "hover-opacity";
+
+  // FontAwesomeのアイコンを追加
+  const currentLocationIcon = document.createElement('i');
+  currentLocationIcon.className = "fa-solid fa-location-crosshairs fa-2xl";
+  currentLocationButton.appendChild(currentLocationIcon);
+
+  // クリックイベントを追加
+  currentLocationButton.addEventListener('click', function() {
+    showLoadingMessage();
+    // ここで現在地を取得してマップを移動させる処理
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        let userLocation = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        };
+        map.setCenter(userLocation);
+        hideLoadingMessage();
+      });
+    }
+  });
+
+  // コントロールをマップに追加
+  map.controls[google.maps.ControlPosition.RIGHT_TOP].push(currentLocationButton);
 }
