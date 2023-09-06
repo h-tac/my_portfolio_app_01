d322cc7d79414a26de9739e5455fc0e855c5f76d
TACHIBANA Hiroki <h.tachi.bana.04@gmail.com>
2023-09-06 14:55:37 +0900

    デフォルトで現在地を表示

--- app/javascript/google_maps_script.js
+++ app/javascript/google_maps_script.js
@@ -1,19 +1,24 @@
-// let map;
-
-// async function initMap() {
-//   const { Map } = await google.maps.importLibrary("maps");
-
-//   map = new Map(document.getElementById("map"), {
-//     center: { lat: 35.686, lng: 139.755 },
-//     zoom: 15,
-//   });
-// }
-
-// initMap();
-
 function initMap() {
   let map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 35.686, lng: 139.755},
     zoom: 15,
   });
+
+  // ユーザーの現在地を取得
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      let userLocation = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+      };
+
+      // マップの中心をユーザーの現在地に設定
+      map.setCenter(userLocation);
+    }, () => {
+      // Geolocationが失敗または拒否された場合、デフォルトの中心を設定
+      map.setCenter({lat: 35.686, lng: 139.755});
+    });
+  } else {
+    // ブラウザがGeolocationをサポートしていない場合、デフォルトの中心を設定
+    map.setCenter({lat: 35.686, lng: 139.755});
+  }
 }
