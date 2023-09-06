e1eab5e1e2f4d74853bfb417967ca72dfad07da6
TACHIBANA Hiroki <h.tachi.bana.04@gmail.com>
2023-09-06 15:57:01 +0900

    マップをロード中のメッセージを追加

--- app/javascript/google_maps_script.js
+++ app/javascript/google_maps_script.js
@@ -1,4 +1,18 @@
+function showLoadingMessage() {
+  const loadingMessage = document.getElementById('loadingMessage');
+  loadingMessage.style.display = 'block';
+  loadingMessage.style.zIndex = '1000';
+}
+
+function hideLoadingMessage() {
+  const loadingMessage = document.getElementById('loadingMessage');
+  loadingMessage.style.display = 'none';
+  loadingMessage.style.zIndex = '';
+}
+
 function initMap() {
+  showLoadingMessage();
+
   let map = new google.maps.Map(document.getElementById('map'), {
     zoom: 15,
   });
@@ -13,12 +27,15 @@ function initMap() {
 
       // マップの中心をユーザーの現在地に設定
       map.setCenter(userLocation);
+      hideLoadingMessage();
     }, () => {
       // Geolocationが失敗または拒否された場合、デフォルトの中心を設定
       map.setCenter({lat: 35.686, lng: 139.755});
+      hideLoadingMessage();
     });
   } else {
     // ブラウザがGeolocationをサポートしていない場合、デフォルトの中心を設定
     map.setCenter({lat: 35.686, lng: 139.755});
+    hideLoadingMessage();
   }
 }
